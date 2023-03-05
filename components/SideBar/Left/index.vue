04af9f4fb2c03920dbe48d0f1e1d35c48e2a6705
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-04 22:09:36 -0500

    refactor: Move component into a file.

--- components/SideBar/Left/index.vue
+++ components/SideBar/Left/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen flex flex-col">
     <div class="p-2 my-2 hover:bg-blue-50 rounded-full w-min dark:hover:bg-white/20"
-    :class="transition"
+         :class="defaultTransition"
     >
       <nuxt-link to="/">
         <div class="w-8 h-8">
@@ -9,12 +9,21 @@
         </div>
       </nuxt-link>
     </div>
+    <SideBarLeftTab active>
+      <template v-slot:icon>
+        <HomeIcon />
+      </template>
+      <template v-slot:name>
+        Home
+      </template>
+    </SideBarLeftTab>
   </div>
 
 </template>
 
 <script setup>
-const transition = 'transition ease-in-out duration-350'
+import {HomeIcon} from '@heroicons/vue/solid'
+const {defaultTransition} = useTailwindConfig()
 </script>
 
 <style scoped>
