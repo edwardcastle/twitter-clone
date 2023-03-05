d6fae0909dcc6ff2b4fb83cb4902c6a336937231
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-04 19:37:54 -0500

    feat: Create twitter logo.

--- app.vue
+++ app.vue
@@ -1,4 +1,20 @@
 <template>
-  <div>
+  <div :class="{'dark': darkMode}">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+          <!-- Left sidebar -->
+          <div class="border-red-500 md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SideBarLeft/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+const darkMode = ref(false)
+</script>
