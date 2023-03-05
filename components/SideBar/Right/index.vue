48bb7b0c18354732cbad0bcf62d5eba0bb3b3bb9
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-05 16:10:43 -0500

    featL Create card on side bar

---
+++ components/SideBar/Right/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Preview Card : What's happening -->
+    <SideBarRightPreviewCard :title="`What's is happening`" />
+    <!-- End Preview Card : What's happening -->
+
+    <!-- Preview Card : Who to follow -->
+    <SideBarRightPreviewCard :title="'Who to follow'" />
+    <!-- End Preview Card : Who to follow -->
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
