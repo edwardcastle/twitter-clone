c4ab4278a5a13b746ac5ad8196507b77d946b268
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-08 18:05:15 -0500

    feat: Create register function

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,10 +1,33 @@
+import { createUser } from "../db/user";
+import { userTransformer } from "~~/server/transformers/user";
 export default defineEventHandler(async (event) => {
-    const body = useBody(event)
-    const {username, email, password, repeatPassword, name} = body
-    if (!username || !email || !password || !repeatPassword || !name) {
-        return sendError(event, createError({statusCode: 400, statusMessage: 'Invalid params'}))
-    }
-    return {
-        body: body
-    }
-})
+  const body = await readBody(event);
+  const { username, email, password, repeatPassword, name } = body;
+  if (!username || !email || !password || !repeatPassword || !name) {
+    return sendError(
+      event,
+      createError({ statusCode: 400, statusMessage: "Invalid params" })
+    );
+  }
+
+  const userData = {
+    username,
+    email,
+    password,
+    name,
+    profileImage: "http://picsum.photos/200/200",
+  };
+
+  if (password !== repeatPassword) {
+    return sendError(
+      event,
+      createError({ statusCode: 400, statusMessage: "Password do not match" })
+    );
+  }
+
+  const user = await createUser(userData);
+
+  return {
+    body: userTransformer(user),
+  };
+});
