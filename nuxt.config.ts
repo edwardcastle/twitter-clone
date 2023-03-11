fc70406eee48d49bb45bfd30f3b9d5bca3eb1ee0
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-11 15:38:20 -0500

    feat: Create access token and refresh token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+require("dotenv").config();
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+  },
+  devServerHandlers: [],
+});
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,31 @@
-export const generateTokens = () => {
+import jwt from "jsonwebtoken";
+
+const generatetAccessToken = (user) => {
+  const config = useRuntimeConfig();
+  return jwt.sign({ userId: user.id }, config.jwtAccessSecret, {
+    expiresIn: "10m",
+  });
+};
+
+const generateRefreshToken = (user) => {
+  const config = useRuntimeConfig();
+  return jwt.sign({ userId: user.id }, config.jwtRefreshSecret, {
+    expiresIn: "4h",
+  });
+};
+
+export const generateTokens = (user) => {
+  const accessToken = generatetAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
   return {
-    accessToken: "",
-    refreshToken: "",
+    accessToken: accessToken,
+    refreshToken: refreshToken,
   };
+};
+// Add http only coockie
+export const sendRefreshToken = (event, token) => {
+  setCookie(event, "refresh_token", token, {
+    httpOnly: true,
+    sameSite: true,
+  });
 };
