b8339d1d8115cac4ea0142e39dd1b0ed6dd1c704
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-11 15:41:21 -0500

    feat: Add send refresh method to login http reques

--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -1,6 +1,9 @@
 import { getUserByUserName } from "../db/user";
 import bcrypt from "bcrypt";
-import { generateTokens } from "~~/utils/jwt";
+import { generateTokens, sendRefreshToken } from "~~/utils/jwt";
+import { userTransformer } from "~~/server/transformers/user";
+import { createRefreshToken } from "../db/refreshTokens";
+import { sendError } from "h3";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
@@ -28,14 +31,29 @@ export default defineEventHandler(async (event) => {
 
   // If the password matches
   const doesThePasswordMatch = await bcrypt.compare(password, user.password);
+  if (!user) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: "Username or password is invalid",
+      })
+    );
+  }
 
   // Generete Tokens
   // Access Tokens
   // Refresh Tokens
-  const { accessToken, refreshToken } = generateTokens();
+  const { accessToken, refreshToken } = generateTokens(user);
+
+  // Save it into the database.
+  await createRefreshToken({ token: refreshToken, userId: user.id });
+
+  // Add http only cookie
+  sendRefreshToken(event, refreshToken);
 
   return {
-    user: user,
-    doesThePasswordMatch,
+    access_token: accessToken,
+    user: userTransformer(user),
   };
 });
